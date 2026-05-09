59480455792c9ad0298710cfa330b0c491002f06
luo Ye <1075560030@qq.com>
2026-05-09 23:57:43 +0800

    feat:  拆分ul

---
+++ src/views/todolist/comonents/List.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  list: Array,
+  removeItem:Function,
+  toggledone:Function
+})
+
+</script>
+
+<template>
+  <ul>
+    <li v-for="(item, index) in list" :key="index">
+      <span :class="{ done: item.done }">
+        {{ item.text }}
+        {{ item.done }}
+      </span>
+      <button @click="removeItem(index)">
+        删除
+      </button>
+      <button @click="toggledone(index)">
+        {{ item.done ? '取消' : '完成' }}
+      </button>
+    </li>
+  </ul>
+</template>
+
+<style lang='scss' scoped></style>
