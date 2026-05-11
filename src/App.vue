31f5874a7f51dcd7dd7142c1a3a61cf0e313dd58
luo Ye <1075560030@qq.com>
2026-05-11 23:45:12 +0800

    feat: v-model

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Todolist from './views/todolist/index.vue'
+import VModel from './views/v-model/index.vue'
 </script>
 
 <template>
   <Todolist />
+  <VModel />
 </template>
 
 <style lang='scss' scoped></style>
